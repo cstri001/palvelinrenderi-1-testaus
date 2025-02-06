0060bc6330c92c335b1d63bac55753a5dab2f73a
Stiina Riihinen <cstri001@edu.xamk.fi>
2025-02-06 23:45:07 +0200

    Add functionality to filter cities

--- index.ts
+++ index.ts
@@ -14,6 +14,23 @@ app.get('/', async (req : express.Request, res : express.Response) => {
   res.render('index', {kunnat : kunnat});
 })
 
+app.get('/hae', async (req : express.Request, res : express.Response) => {
+  // This req.query.enter-text-here is the part in the URL that's like .../hae?hakusana=Mikkeli
+  // Force-casting this as string is required to prevent errors here
+  console.log(req.query.hakusana)
+  let hakusana : string = req.query.hakusana as string;
+  let kunnat = await prisma.kunta.findMany({
+    where: {
+      kunta: {
+        startsWith: hakusana
+      }
+    }
+  })
+
+  // Remember to send a response here as well, with the newly fetched database info!
+  res.render('index', {kunnat: kunnat})
+})
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 })
